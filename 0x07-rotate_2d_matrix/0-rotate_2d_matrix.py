d9db6730d6a470c22a9c8c7bfc9906cf852be6b0
Imukua <ianmukuaa@gmail.com>
2024-02-23 22:45:15 +0300

    done and dusted

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""rotation of atrix
+"""
+
+
+def transpose_matrix(matrix, n):
+    """
+
+    Args:
+        matrix (_type_): _description_
+    """
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+def reverse_matrix(matrix):
+    """
+
+    Args:
+        matrix (_type_): _description_
+    """
+    for row in matrix:
+        row.reverse()
+
+
+def rotate_2d_matrix(matrix):
+    """
+
+    Args:
+        matrix (_type_): _description_
+    """
+    n = len(matrix)
+
+    """sample matrix
+    1 2 3
+    4 5 6
+    7 8 9
+    """
+
+    """
+    1 4 7
+    2 5 8
+    3 6 9
+    """
+
+    transpose_matrix(matrix, n)
+
+    """
+    7 4 1
+    8 5 2
+    9 6 3
+    """
+    reverse_matrix(matrix)
+
+    return matrix
